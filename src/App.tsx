97a0497af12f54430143006be3c8cab4e9b288af
saagarbhujel <saagarbhujel22@gmail.com>
2023-11-10 22:45:17 +0545

    route added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
-
+import {Route, Routes} from "react-router-dom";
+import AuthLayout from "./_auth/AuthLayout.tsx";
+import SignInForm from "./_auth/forms/SignInForm.tsx";
+import SignUpForm from "./_auth/forms/SignUpForm.tsx";
 
 
 function App() {
 
 
   return (
-    <>
-      home
-    </>
+   <main className={'flex h-screen'}>
+       <Routes>
+           {/*Public Routes*/}
+           <Route element={<AuthLayout/>}>
+               <Route path={'sign-in'} element={<SignInForm/>} />
+               <Route path={'sign-up'} element={<SignUpForm/>} />
+           </Route>
+
+              {/*Private Routes*/}
+
+
+       </Routes>
+
+   </main>
   )
 }
 
