58675856a06dedf57a7c9e613a0e81f3b9ae9a78
saagarbhujel <saagarbhujel22@gmail.com>
2023-11-10 22:43:58 +0545

    tailwind added

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,14 +1,15 @@
 // jest.config.js
 module.exports = {
-
     preset: 'ts-jest',
     testEnvironment: 'jsdom',
     setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
-
     moduleNameMapper: {
         '^@/(.*)$': '<rootDir>/src/$1',
     },
     testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+    transform: {
+    '^.+\\.tsx?$':'ts-jest'
+    },
     globals: {
         'ts-jest': {
             tsconfig: 'tsconfig.jest.json',
