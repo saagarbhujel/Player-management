186e3fbd81d3d8ec7d891f9b90fb23be38073fba
saagarbhujel <saagarbhujel22@gmail.com>
2023-11-10 22:45:43 +0545

    layout for auth done

---
+++ src/_auth/AuthLayout.tsx
@@ -0,0 +1,21 @@
+import {Navigate, Outlet} from "react-router-dom";
+
+const AuthLayout = () => {
+
+    const isAuthenticated = false;
+    return (
+        <>
+            {
+                isAuthenticated ? (
+                    <Navigate to={'/'} />
+                ) : (
+                    <section>
+                        <Outlet />
+                    </section>
+                )
+            }
+        </>
+    );
+};
+
+export default AuthLayout;
