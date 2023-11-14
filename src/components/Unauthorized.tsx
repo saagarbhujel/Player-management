23bdb629f32fbd131ee3b0cf4b48887206871821
saagarbhujel <saagarbhujel22@gmail.com>
2023-11-14 14:35:56 +0545

    unauthorized component done

---
+++ src/components/Unauthorized.tsx
@@ -0,0 +1,12 @@
+
+
+const Unauthorized = () => {
+  return (
+    <div className="flex flex-col justify-center w-full items-center text-2xl">
+    <span>Unauthorized</span>
+    <span className="text-gray-400 text-md">You're not authorized to view this page.</span>
+  </div>
+  )
+}
+
+export default Unauthorized
