b3fbd32185440bae49aab4d7749c4f542bf8980a
saagarbhujel <saagarbhujel22@gmail.com>
2023-11-14 16:24:47 +0545

    wrapped with persist login component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import {Route, Routes} from "react-router-dom";
 import AuthLayout from "./_auth/AuthLayout.tsx";
 import SignInForm from "./_auth/forms/SignInForm.tsx";
 import SignUpForm from "./_auth/forms/SignUpForm.tsx";
+import Home from "./pages/Home.tsx";
+import RequireAuth from "./components/RequireAuth.tsx";
+import PersistLogin from "./components/PersistLogin.tsx";
 
 
 function App() {
@@ -10,6 +13,9 @@ function App() {
   return (
    <main className={'flex h-screen'}>
        <Routes>
+        <Route element={<PersistLogin/>}>
+
+        
            {/*Public Routes*/}
            <Route element={<AuthLayout/>}>
                <Route path={'sign-in'} element={<SignInForm/>} />
@@ -17,8 +23,11 @@ function App() {
            </Route>
 
               {/*Private Routes*/}
+              <Route element={<RequireAuth roles={['admin', 'staff', 'player']} />} >
+                <Route path="/" element={<Home/>} />
+              </Route>
 
-
+              </Route>
        </Routes>
 
    </main>
