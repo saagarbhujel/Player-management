9dc005d3fba2f0af0fc9b10752062599a838077e
saagarbhujel <saagarbhujel22@gmail.com>
2023-11-15 17:47:16 +0545

    constants added

---
+++ src/constants/index.ts
@@ -0,0 +1,29 @@
+export const sidebarLinks = [
+    {
+    //   imageUrl: "/assets/icons/home.svg",
+      route: "/",
+      label: "Home",
+    },
+    {
+    //   imageUrl: '/assets/icons/wallpaper.svg',
+      route: '/explore',
+      label: 'Explore',
+    },
+    {
+    //   imageUrl: '/assets/icons/people.svg',
+      route: '/all-user',
+      label: 'People',
+    },
+    {
+    //   imageUrl: '/assets/icons/bookmark.svg',
+      route: '/saved',
+      label: 'Saved',
+    },
+    {
+    //   imageUrl: '/assets/icons/gallery-add.svg',
+      route: '/create-post',
+      label: 'Create Post',
+    }
+  ];
+  
+  
