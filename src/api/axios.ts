868d993111f9eaa146eb8849a04a86087a9cd533
saagarbhujel <saagarbhujel22@gmail.com>
2023-11-16 23:49:36 +0545

    axios private function added

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -8,3 +8,11 @@ export default axios.create({
         'Content-type': 'application/json',
     },
 });
+
+export const axiosPrivate = axios.create({
+    baseURL: BASE_URL,
+    headers: {
+        'Content-type': 'application/json',
+    },
+    withCredentials: true,
+})
