9de112f427f286427809ac3b982a4469b9c619fc
saagarbhujel <saagarbhujel22@gmail.com>
2023-11-16 23:48:27 +0545

    navbar added

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,17 +1,18 @@
-
-import LeftSideBar from '../components/LeftSideBar'
-import { Outlet } from 'react-router-dom'
+import LeftSideBar from "../components/LeftSideBar";
+import { Outlet } from "react-router-dom";
+import NavBar from "../components/NavBar";
 
 const RootLayout = () => {
   return (
-  <div className="w-full md:flex">
-    <LeftSideBar/>
+    <div className="w-full md:flex">
+      <LeftSideBar />
 
-    <section>
-        <Outlet/>
-    </section>
-  </div> 
-  )
-}
+      <section className="flex flex-col flex-1 h-full">
+        <NavBar />
+        <Outlet />
+      </section>
+    </div>
+  );
+};
 
-export default RootLayout
+export default RootLayout;
