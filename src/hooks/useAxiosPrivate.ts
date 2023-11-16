a241dfcf0f810b56345f30da345f331d7d012885
saagarbhujel <saagarbhujel22@gmail.com>
2023-11-16 23:51:08 +0545

    axiosPrvate hook to refresh token

---
+++ src/hooks/useAxiosPrivate.ts
@@ -0,0 +1,51 @@
+
+import useRefrehToken from './useRefrehToken'
+import useAuth from './useAuth';
+import { axiosPrivate } from '../api/axios';
+
+const useAxiosPrivate = () => {
+
+    const refresh = useRefrehToken();
+    const {auth} = useAuth();
+
+    const requestInterceptor = axiosPrivate.interceptors.request.use(
+        (config) => {
+            if(!config.headers['Authorization']){
+                config.headers['Authorization'] = `Bearer ${auth.accessToken}`
+            }
+            return config;
+        },
+        (error) => Promise.reject(error)
+    )
+
+    const responseInterceptor = axiosPrivate.interceptors.response.use(
+        (response) => response,
+        async (error) => {
+            const prevRequest = error.config;
+            if(error.response.status === 401 && !prevRequest._isRetry ){
+                prevRequest._isRetry = true;
+                try {
+                    const newAccessToken = await refresh();
+                    prevRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
+                    return axiosPrivate(prevRequest);
+                } catch (error) {
+                    console.log(error);
+                    
+                }
+            }
+            if(!prevRequest._skipInterceptor){
+                return Promise.reject(error);
+            }
+        }
+    )
+
+    return () => {
+        axiosPrivate.interceptors.request.eject(requestInterceptor);
+        axiosPrivate.interceptors.response.eject(responseInterceptor);
+    }
+
+
+ 
+}
+
+export default useAxiosPrivate
