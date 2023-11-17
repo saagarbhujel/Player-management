331b3655877fd6738f2f761e5532a740804bf2f0
saagarbhujel <saagarbhujel22@gmail.com>
2023-11-17 17:52:36 +0545

    formatted

--- src/hooks/useAxiosPrivate.ts
+++ src/hooks/useAxiosPrivate.ts
@@ -1,51 +1,49 @@
-
-import useRefrehToken from './useRefrehToken'
-import useAuth from './useAuth';
-import { axiosPrivate } from '../api/axios';
+import useRefrehToken from "./useRefrehToken";
+import useAuth from "./useAuth";
+import { axiosPrivate } from "../api/axios";
+import { useEffect } from "react";
 
 const useAxiosPrivate = () => {
+  const refresh = useRefrehToken();
+  const { auth } = useAuth();
 
-    const refresh = useRefrehToken();
-    const {auth} = useAuth();
-
+  useEffect(() => {
     const requestInterceptor = axiosPrivate.interceptors.request.use(
-        (config) => {
-            if(!config.headers['Authorization']){
-                config.headers['Authorization'] = `Bearer ${auth.accessToken}`
-            }
-            return config;
-        },
-        (error) => Promise.reject(error)
-    )
+      (config) => {
+        if (!config.headers["Authorization"]) {
+          config.headers["Authorization"] = `Bearer ${auth.accessToken}`;
+        }
+        return config;
+      },
+      (error) => Promise.reject(error)
+    );
 
     const responseInterceptor = axiosPrivate.interceptors.response.use(
-        (response) => response,
-        async (error) => {
-            const prevRequest = error.config;
-            if(error.response.status === 401 && !prevRequest._isRetry ){
-                prevRequest._isRetry = true;
-                try {
-                    const newAccessToken = await refresh();
-                    prevRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
-                    return axiosPrivate(prevRequest);
-                } catch (error) {
-                    console.log(error);
-                    
-                }
-            }
-            if(!prevRequest._skipInterceptor){
-                return Promise.reject(error);
-            }
+      (response) => response,
+      async (error) => {
+        const prevRequest = error.config;
+        if (error.response.status === 401 && !prevRequest._isRetry) {
+          prevRequest._isRetry = true;
+          try {
+            const newAccessToken = await refresh();
+            prevRequest.headers["Authorization"] = `Bearer ${newAccessToken}`;
+            return axiosPrivate(prevRequest);
+          } catch (error) {
+            console.log(error);
+          }
         }
-    )
+        if (!prevRequest._skipInterceptor) {
+          return Promise.reject(error);
+        }
+      }
+    );
 
     return () => {
-        axiosPrivate.interceptors.request.eject(requestInterceptor);
-        axiosPrivate.interceptors.response.eject(responseInterceptor);
-    }
+      axiosPrivate.interceptors.request.eject(requestInterceptor);
+      axiosPrivate.interceptors.response.eject(responseInterceptor);
+    };
+  }, [auth, refresh]);
+  return axiosPrivate;
+};
 
-
- 
-}
-
-export default useAxiosPrivate
+export default useAxiosPrivate;
