dafdb912acaba1757c62e52428d70639b97bdfe1
saagarbhujel <saagarbhujel22@gmail.com>
2023-11-19 20:14:34 +0545

    play game done

--- src/_root/pages/Games.tsx
+++ src/_root/pages/Games.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import { useState } from "react";
+import useAuth from "../../hooks/useAuth";
+import useAxiosPrivate from "../../hooks/useAxiosPrivate";
+import Loader from "../../components/Loader";
+import { useToast } from "../../hooks/useToast";
 
 const Games = () => {
-  return (
-    <div>Games</div>
-  )
-}
+  const { user } = useAuth();
+  const axiosPrivate = useAxiosPrivate();
+  const { setToast } = useToast();
 
-export default Games
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const handlePlayGame = async () => {
+    // console.log('play game')
+    setIsLoading(true);
+    try {
+      const res = await axiosPrivate.get("/player/play/game");
+      // console.log(res.data.message);
+      const message = res.data.message;
+      setToast(message, "info");
+    } catch (error) {
+      // console.log(error);
+      setToast("Something went wrong. Please try again later.", "error");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className={`${user.role !== "player" && "hidden"}`}>
+        <button
+          onClick={handlePlayGame}
+          className=" px-8 py-3 bg-blue-500/50 hover:bg-blue-500/60 rounded-lg"
+        >
+          {isLoading ? <Loader /> : "Play"}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Games;
