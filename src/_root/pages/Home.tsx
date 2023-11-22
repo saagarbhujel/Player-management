4d67a473b1eb3990dbe7e3cea6cdc67415147099
saagarbhujel <saagarbhujel22@gmail.com>
2023-11-22 18:35:09 +0545

    conditional rendering of component added

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,14 +1,22 @@
-import LeaderBoard from "../../components/LeaderBoard"
-
+import LeaderBoard from "../../components/LeaderBoard";
+import useAuth from "../../hooks/useAuth";
 
 const Home = () => {
+  const { user } = useAuth();
+  const role = user.role;
   document.title = "Player Management-Home";
-   
-  return (
-    <div >
-     <LeaderBoard/>
-    </div>
-  )
-}
 
-export default Home
+  return (
+    <div>
+      {role === "admin" ? (
+        <div>Admin</div>
+      ) : role === "staff" ? (
+        <div>Staff</div>
+      ) : (
+        <LeaderBoard />
+      )}
+    </div>
+  );
+};
+
+export default Home;
