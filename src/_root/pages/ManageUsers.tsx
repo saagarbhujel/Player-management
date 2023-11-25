d9d82b505339ed99a5efd407c437bbc38ed63d40
saagarbhujel <saagarbhujel22@gmail.com>
2023-11-25 23:53:23 +0545

    manage user, tab done

--- src/_root/pages/ManageUsers.tsx
+++ src/_root/pages/ManageUsers.tsx
@@ -1,10 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const ManageUsers = () => {
 
-  document.title = 'Player Management-Manage Users'
+  const [active, setActive] = useState('users');
+
+  if(active === 'users') {
+    document.title = 'Player Management- All Users'
+  } else {
+    document.title = 'Player Management- Create User'
+  }
+  // document.title = 'Player Management-Manage Users'
   return (
-    <div>ManageUsers</div>
+    <div className='flex m-auto flex-col w-[70vw] mt-2'>
+      <div>
+        <ul className='flex w-full  bg-green-300/30 rounded-md '>
+          <button 
+          type='button'
+          className={` ${active === 'users' ? 'text-white bg-blue-500 rounded-l-md py-3 ' : ""} flex-1`}
+          onClick={() => setActive('users')}
+          >
+            All Users
+          </button>
+
+          <button className={`${active === 'createUser' ? 'text-white bg-blue-500 rounded-r-md py-3 ' : "" } flex-1`}
+          onClick={() => setActive('createUser')}
+          >
+            Create User
+          </button>
+        </ul>
+      </div>
+      <div>
+        {
+          active === 'users' ? (
+            <div>
+              <h2>All Users</h2>
+            </div>
+          ): (
+            <div>
+              <h2>Create User</h2>
+            </div>
+          )
+        }
+      </div>
+    </div>
   )
 }
 
