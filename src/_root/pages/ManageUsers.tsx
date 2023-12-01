f6ce84d6872833c690567fa812d018514f0121be
saagarbhujel <saagarbhujel22@gmail.com>
2023-12-01 21:23:43 +0545

    unused import removed

--- src/_root/pages/ManageUsers.tsx
+++ src/_root/pages/ManageUsers.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import AllUsers from "../../components/AllUsers";
 import CreateUsers from "../../components/CreateUsers";
 
