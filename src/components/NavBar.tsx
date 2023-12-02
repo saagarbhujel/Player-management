47427299658d521976640a655d342580b9199b3f
saagarbhujel <saagarbhujel22@gmail.com>
2023-12-02 23:55:50 +0545

    searchbar added

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,17 @@
+import useAuth from "../hooks/useAuth"
+import SearchBar from "./SearchBar"
 
 const NavBar = () => {
+
+  const {user} = useAuth()
   return (
-    <div className='w-full h-16 flex items-center'>NavBar</div>
+    <div className='w-full h-16 flex items-center justify-center z-20'>
+      {
+        user.role === ('admin' || "staff") ? (
+          <SearchBar />
+        ):(null)
+      }
+    </div>
   )
 }
 
