be09cf75c702fb245c1ec2bf78f04521c243061f
saagarbhujel <saagarbhujel22@gmail.com>
2024-01-09 23:39:38 +0545

    side nav for small devices add

--- src/components/LeftSideBar.tsx
+++ src/components/LeftSideBar.tsx
@@ -6,6 +6,7 @@ import ConformModal from "../_root/pages/ConformModal";
 import useAuth from "../hooks/useAuth";
 import Loader from "./Loader";
 import useDetails from "../hooks/useDetails";
+import { IoMdClose } from "react-icons/io";
 
 const LeftSideBar = () => {
   const { pathname } = useLocation();
@@ -20,9 +21,13 @@ const LeftSideBar = () => {
   // console.log( playerDetails);
 
   const [isLogoutModalOpen, setIsLogoutModalOpen] = useState(false);
+  const [isLeftSideBarOpen, setIsLeftSideBarOpen] = useState(false);
+
+
 
   if (role === "player") {
     return (
+      <>
       <nav className="leftsidebar max-h-full bg-gray-100">
         <div className=" flex flex-col gap-11">
           {!user.id ? (
@@ -75,7 +80,98 @@ const LeftSideBar = () => {
           </ul>
         </div>
         <button
-          className="shad-button_ghost"
+          className="flex gap-4 items-center p-4 bg-orange-400/90 rounded-md hover:bg-orange-500"
+          onClick={() => {
+            setIsLogoutModalOpen(true);
+          }}
+        >
+          <img src="/assets/icons/logout.svg" alt="logout" />
+          <p className=" small-medium lg:base-medium">Logout</p>
+        </button>
+      </nav>
+
+      
+        {/* // mobile nav */}
+        <section className="md:hidden " >
+          <div className="flex  absolute z-30  md:hidden justify-between items-center top-0  h-16   px-5  ">
+
+          <button
+          className={` text-2xl `}
+          onClick={()=>{
+            setIsLeftSideBarOpen(!isLeftSideBarOpen)
+          }}>
+             ☰
+          </button>
+          </div>
+
+
+          <div 
+          className={`${isLeftSideBarOpen ?"translate-x-0 " : "  translate-x-[-100%]"} left-0  transition-all fixed duration-300  h-full  z-30 `}
+          >
+           
+          <nav className="flex px-6 py-10 flex-col justify-between min-w-[220px] max-h-full  bg-gray-100 h-full">
+          <div className=" absolute right-0 top-0 m-2 p-1">
+             <button 
+             className="text-2xl"
+             onClick={()=>{
+              setIsLeftSideBarOpen(!isLeftSideBarOpen)
+             }}
+             >
+          <IoMdClose />
+             </button>
+            </div>
+        <div className=" flex flex-col gap-11">
+          {!user.id ? (
+            <div className=" h-14">
+              <Loader />
+            </div>
+          ) : (
+            <Link
+              to={`/profile/${user.id}`}
+              className="flex gap-3 items-center"
+            >
+              {!playerDetails ? (
+                <Loader />
+              ) : (
+                <>
+                  <div className="bg-gray-500 w-12 h-12 rounded-full" />
+                  <div className="flex flex-col">
+                    <p className="body-bold first-letter:capitalize">
+                      {playerDetails?.name}
+                    </p>
+                    <p className="small-regular text-light-3">
+                      {CountryMap.get(playerDetails?.country)}
+                    </p>
+                  </div>
+                </>
+              )}
+            </Link>
+          )}
+
+          <ul>
+            {sidebarLinks.map((link: INavLink) => {
+              const isActive = pathname === link.route;
+              return (
+                <li
+                  className={`leftsidebar-link group ${
+                    isActive && "bg-primary-500"
+                  }`}
+                  key={link.label}
+                >
+                  <NavLink
+                    className={"flex gap-4 items-center p-4"}
+                    to={link.route}
+                  >
+                    {/* <img src={link.imageUrl} alt={link.label} className={` group-hover:invert-white ${isActive && 'invert-white'}`} /> */}
+                    {link.label}
+                  </NavLink>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <button
+          className="flex gap-4 items-center p-4 bg-orange-400/90 rounded-md hover:bg-orange-500"
           onClick={() => {
             setIsLogoutModalOpen(true);
           }}
@@ -84,8 +180,12 @@ const LeftSideBar = () => {
           <p className=" small-medium lg:base-medium">Logout</p>
         </button>
 
+      </nav>
+          </div>
+
+        </section>
         <ConformModal
-          className="top-[45vh] left-[50rem]"
+          className=" left-[5rem] top-[45vh] md:left-[50rem]"
           isOpened={isLogoutModalOpen}
           message="Are you sure you want to logout?"
           onConfirm={() => {
@@ -97,12 +197,15 @@ const LeftSideBar = () => {
             setIsLogoutModalOpen(false);
           }}
         />
-      </nav>
+
+        </>
     );
   }
 
   if (role === "admin" || role === "staff") {
     return (
+      <>
+      
       <nav className="leftsidebar max-h-full bg-gray-100">
         <div className=" flex flex-col gap-11">
           {!user.id ? (
@@ -146,7 +249,7 @@ const LeftSideBar = () => {
           </ul>
         </div>
         <button
-          className="shad-button_ghost"
+          className="flex gap-4 items-center p-4 bg-orange-400/90 rounded-md hover:bg-orange-500 "
           onClick={() => {
             setIsLogoutModalOpen(true);
           }}
@@ -155,8 +258,101 @@ const LeftSideBar = () => {
           <p className=" small-medium lg:base-medium">Logout</p>
         </button>
 
+       
+
+        
+      </nav>
+
+
+      {/* // mobile nav */}
+      <section className="md:hidden " >
+          <div className="flex  absolute z-30  md:hidden justify-between items-center top-0  h-16   px-5  ">
+
+          <button
+          className={` text-2xl `}
+          onClick={()=>{
+            setIsLeftSideBarOpen(!isLeftSideBarOpen)
+          }}>
+             ☰
+          </button>
+          </div>
+
+
+          <div 
+          className={`${isLeftSideBarOpen ?"translate-x-0 " : "  translate-x-[-100%]"} left-0  transition-all fixed duration-300  h-full  z-30 `}
+          >
+           
+          <nav className="flex px-6 py-10 flex-col justify-between min-w-[220px] max-h-full  bg-gray-100 h-full">
+          <div className=" absolute right-0 top-0 m-2 p-1">
+             <button 
+             className="text-2xl"
+             onClick={()=>{
+              setIsLeftSideBarOpen(!isLeftSideBarOpen)
+             }}
+             >
+          <IoMdClose />
+             </button>
+            </div>
+        
+     
+            <div className=" flex flex-col gap-11">
+          {!user.id ? (
+            <div className=" h-14">
+              <Loader />
+            </div>
+          ) : (
+            <div className="flex gap-3 items-center">
+              <div className=" w-12 h-12 rounded-full">
+                <img
+                  src="/assets/icons/profile-placeholder.svg"
+                  alt="profile"
+                />
+              </div>
+              <div className="flex flex-col">
+                <p className="body-bold first-letter:capitalize">{role}</p>
+              </div>
+            </div>
+          )}
+
+          <ul>
+            {adminSidebarLinks.map((link: INavLink, index) => {
+              const isActive = pathname === link.route;
+              return (
+                <li
+                  className={`leftsidebar-link group ${
+                    isActive && "bg-primary-500"
+                  }`}
+                  key={index}
+                >
+                  <NavLink
+                    className={"flex gap-4 items-center p-4"}
+                    to={link.route}
+                  >
+                    {/* <img src={link.imageUrl} alt={link.label} className={` group-hover:invert-white ${isActive && 'invert-white'}`} /> */}
+                    {link.label}
+                  </NavLink>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <button
+          className="flex gap-4 items-center p-4 bg-orange-400/90 rounded-md hover:bg-orange-500 "
+          onClick={() => {
+            setIsLogoutModalOpen(true);
+          }}
+        >
+          <img src="/assets/icons/logout.svg" alt="logout" />
+          <p className=" small-medium lg:base-medium">Logout</p>
+        </button>
+
+        
+      </nav>
+          </div>
+
+        </section>
         <ConformModal
-          className="top-[45vh] left-[50rem]"
+          className="left-[5rem] top-[45vh] md:left-[50rem]"
           isOpened={isLogoutModalOpen}
           message="Are you sure you want to logout?"
           onConfirm={() => {
@@ -168,7 +364,7 @@ const LeftSideBar = () => {
             setIsLogoutModalOpen(false);
           }}
         />
-      </nav>
+      </>
     );
   }
 };
